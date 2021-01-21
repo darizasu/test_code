1ff8fdfda1e26675f525d9981959f023f5d2e773
Daniel Ariza Suarez <daarizasu@unal.edu.co>
2021-01-21 13:03:44 -0500

    Create problem1_sample_input.R

---
+++ problem1_sample_input.R
@@ -0,0 +1,7 @@
+n <- 5
+k <- 3
+
+message('Return the total number of rabbit pairs that will be present after','\n',
+        n, ' months, if we begin with 1 pair and in each generation,','\n',
+        'every pair of reproduction-age rabbits produces a litter of ', k,'\n',
+        'rabbit pairs (instead of only 1 pair).')
